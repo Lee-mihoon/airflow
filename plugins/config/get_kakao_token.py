d1270b0af34688edc99420bdcf7b9e22726b7078
mihoon <meehoon0731@naver.com>
2025-07-09 10:05:27 +0900

    teardown 실습용'

--- plugins/config/get_kakao_token.py
+++ plugins/config/get_kakao_token.py
@@ -1,9 +1,9 @@
 import requests
 # client_id, authorize_code 노출 주의, 실제 값은 임시로만 넣고 Git에 올라가지 않도록 유의
 
-client_id = '{client_id}'
+client_id = '1377c40d1eb13eb53cfa54bc3d60aa7b'
 redirect_uri = 'https://example.com/oauth'
-authorize_code = '{authorize_code}'
+authorize_code = 's7zgKYaf_O0II46GvCdtRYc1dFYiL8FeEkSTp_hc9EHB6o5HKSGawwAAAAQKDSAbAAABl4sF6DktjdRiIM79qQ'
 
 
 token_url = 'https://kauth.kakao.com/oauth/token'
